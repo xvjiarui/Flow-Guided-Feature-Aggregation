75ce2979ee8d0931bdb2515cf882641e7a959db2
Jiarui XU <xvjiarui0826@gmail.com>
2018-07-09 22:52:37 +0800

    add rcnn

--- rcnn/train_end2end.py
+++ rcnn/train_end2end.py
@@ -1,14 +1,9 @@
 # --------------------------------------------------------
-# Flow-Guided Feature Aggregation
-# Copyright (c) 2017 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Modified by Yuqing Zhu, Shuhao Fu, Yuwen Xiong
-# --------------------------------------------------------
-# Based on:
-# MX-RCNN
+# Deep Feature Flow
 # Copyright (c) 2016 by Contributors
-# Licence under The Apache 2.0 License
-# https://github.com/ijkguo/mx-rcnn/
+# Copyright (c) 2017 Microsoft
+# Licensed under The Apache-2.0 License [see LICENSE for details]
+# Modified by Yuwen Xiong
 # --------------------------------------------------------
 
 import _init_paths
@@ -35,8 +30,6 @@ def parse_args():
 
     # modfication for philly
     if config.USE_PHILLY:
-        # not working actually
-        config.gpus = '0,1,2,3,4,5,6,7'
         parser.add_argument('--dataDir', help='input directory for Philly jobs', required=True, type=str)
         parser.add_argument('--modelDir', help='output directory for Philly jobs', required=True, type=str)
         args, rest = parser.parse_known_args()
@@ -66,14 +59,15 @@ from utils.load_model import load_param
 from utils.PrefetchingIter import PrefetchingIter
 from utils.lr_scheduler import WarmupMultiFactorScheduler
 
-def train_net(args, ctx, pretrained_dir, pretrained_resnet, pretrained_flow, epoch, prefix, begin_epoch, end_epoch, lr, lr_step):
+
+def train_net(args, ctx, pretrained_dir, pretrained_resnet, epoch, prefix, begin_epoch, end_epoch, lr, lr_step):
     logger, final_output_path = create_logger(config.output_path, args.cfg, config.dataset.image_set)
     prefix = os.path.join(final_output_path, prefix)
 
     # load symbol
     shutil.copy2(os.path.join(curr_path, 'symbols', config.symbol + '.py'), final_output_path)
     sym_instance = eval(config.symbol + '.' + config.symbol)()
-    sym = sym_instance.get_train_symbol(config)
+    sym = sym_instance.get_symbol(config, is_train=True)
     feat_sym = sym.get_internals()['rpn_cls_score_output']
 
     # setup multi-gpu
@@ -91,7 +85,7 @@ def train_net(args, ctx, pretrained_dir, pretrained_resnet, pretrained_flow, epo
     # load dataset and prepare imdb for training
     image_sets = [iset for iset in config.dataset.image_set.split('+')]
     roidbs = [load_gt_roidb(config.dataset.dataset, image_set, config.dataset.root_path, config.dataset.dataset_path,
-                            flip=config.TRAIN.FLIP, use_philly=config.USE_PHILLY)
+                            flip=config.TRAIN.FLIP)
               for image_set in image_sets]
     roidb = merge_roidb(roidbs)
     roidb = filter_roidb(roidb, config)
@@ -103,13 +97,10 @@ def train_net(args, ctx, pretrained_dir, pretrained_resnet, pretrained_flow, epo
                               bbox_std=config.network.ANCHOR_STDS)
 
     # infer max shape
-    # times 2 here for double images
-    max_data_shape = [('data', (2*config.TRAIN.BATCH_IMAGES, 3, max([v[0] for v in config.SCALES]), max([v[1] for v in config.SCALES]))),]
+    max_data_shape = [('data', (config.TRAIN.BATCH_IMAGES, 3, max([v[0] for v in config.SCALES]), max([v[1] for v in config.SCALES])))]
     max_data_shape, max_label_shape = train_data.infer_shape(max_data_shape)
     max_data_shape.append(('gt_boxes', (config.TRAIN.BATCH_IMAGES, 100, 5)))
-    max_data_shape.append(('ref_gt_boxes', (config.TRAIN.BATCH_IMAGES, 100, 5)))
-
-    print 'providing maximum shape', max_data_shape, max_label_shape
+    print('providing maximum shape', max_data_shape, max_label_shape)
 
     data_shape_dict = dict(train_data.provide_data_single + train_data.provide_label_single)
     pprint.pprint(data_shape_dict)
@@ -124,17 +115,15 @@ def train_net(args, ctx, pretrained_dir, pretrained_resnet, pretrained_flow, epo
                         logger=logger, context=ctx, max_data_shapes=[max_data_shape for _ in range(batch_size)],
                         max_label_shapes=[max_label_shape for _ in range(batch_size)], fixed_param_prefix=fixed_param_prefix)
 
+
     # load and initialize params
     params_loaded = False
     if config.TRAIN.RESUME:
         arg_params, aux_params = load_param(prefix, begin_epoch, convert=True)
-        # mod._preload_opt_states = '{}-{:04d}.states'.format(prefix, begin_epoch)
+        mod._preload_opt_states = '%s-%04d.states'%(prefix, begin_epoch)
         print('continue training from ', begin_epoch)
         logger.info('continue training from ', begin_epoch)
         params_loaded = True
-        sym_instance.init_weight(config, arg_params, aux_params)
-        print('init nms')
-        logger.info('init nms')
     elif config.TRAIN.AUTO_RESUME:
         for cur_epoch in range(end_epoch-1, begin_epoch, -1):
             params_filename = '{}-{:04d}.params'.format(prefix, cur_epoch)
@@ -149,22 +138,9 @@ def train_net(args, ctx, pretrained_dir, pretrained_resnet, pretrained_flow, epo
                 break
     if not params_loaded:
         arg_params, aux_params = load_param(os.path.join(pretrained_dir, pretrained_resnet), epoch, convert=True)
-        arg_params_flow, aux_params_flow = load_param(os.path.join(pretrained_dir, pretrained_flow), epoch, convert=True)
-        arg_params.update(arg_params_flow)
-        aux_params.update(aux_params_flow)
-        sym_instance.init_weight(config, arg_params, aux_params)
 
-    # if config.TRAIN.RESUME:
-    #     print('continue training from ', begin_epoch)
-    #     arg_params, aux_params = load_param(prefix, begin_epoch, convert=True)
-    # else:
-    #     arg_params, aux_params = load_param(pretrained, epoch, convert=True)
-    #     arg_params_flow, aux_params_flow = load_param(pretrained_flow, epoch, convert=True)
-    #     arg_params.update(arg_params_flow)
-    #     aux_params.update(aux_params_flow)
 
-    #     sym_instance.init_weight(config, arg_params, aux_params)
-
+    sym_instance.init_weight(config, arg_params, aux_params)
     # check parameter shapes
     sym_instance.check_parameter_shapes(arg_params, aux_params, data_shape_dict)
 
@@ -189,7 +165,13 @@ def train_net(args, ctx, pretrained_dir, pretrained_resnet, pretrained_flow, epo
         eval_metrics.add(metric.NMSAccMetric(config))
 
     # callback
-    batch_end_callback = callback.Speedometer(train_data.batch_size, frequent=args.frequent)
+    batch_end_callback = [callback.Speedometer(train_data.batch_size, frequent=args.frequent)]
+
+    if config.USE_PHILLY:
+        total_iter = (end_epoch - begin_epoch) * len(roidb) / input_batch_size
+        progress_frequent = min(args.frequent * 10, 100)
+        batch_end_callback.append(callback.PhillyProgressCallback(total_iter, progress_frequent))
+        
     means = np.tile(np.array(config.TRAIN.BBOX_MEANS), 2 if config.CLASS_AGNOSTIC else config.dataset.NUM_CLASSES)
     stds = np.tile(np.array(config.TRAIN.BBOX_STDS), 2 if config.CLASS_AGNOSTIC else config.dataset.NUM_CLASSES)
     epoch_end_callback = [mx.callback.module_checkpoint(mod, prefix, period=1, save_optimizer_states=True), callback.do_checkpoint(prefix, means, stds)]
@@ -217,18 +199,13 @@ def train_net(args, ctx, pretrained_dir, pretrained_resnet, pretrained_flow, epo
     mod.fit(train_data, eval_metric=eval_metrics, epoch_end_callback=epoch_end_callback,
             batch_end_callback=batch_end_callback, kvstore=config.default.kvstore,
             optimizer='sgd', optimizer_params=optimizer_params,
-            arg_params=arg_params, aux_params=aux_params, begin_epoch=begin_epoch, num_epoch=end_epoch)
+            arg_params=arg_params, aux_params=aux_params, begin_epoch=begin_epoch, num_epoch=end_epoch - begin_epoch)
 
 
 def main():
-    print("Pulled from current commit:")
-    os.system('git rev-parse HEAD')
     print('Called with argument:', args)
-    if args.usePhilly:
-        config.gpus ='0,1,2,3,4,5,6,7'
     ctx = [mx.gpu(int(i)) for i in config.gpus.split(',')]
-
-    train_net(args, ctx, config.network.pretrained_dir, config.network.pretrained_resnet, config.network.pretrained_flow, config.network.pretrained_epoch, config.TRAIN.model_prefix,
+    train_net(args, ctx, config.network.pretrained_dir, config.network.pretrained_resnet, config.network.pretrained_epoch, config.TRAIN.model_prefix,
               config.TRAIN.begin_epoch, config.TRAIN.end_epoch, config.TRAIN.lr, config.TRAIN.lr_step)
 
 if __name__ == '__main__':
